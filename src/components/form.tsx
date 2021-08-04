71bb66caa4d888f01337ae565d4e1cf48e758ec4
charlotte@virtualtrips.io <charlotte@virtualtrips.io>
2021-08-04 13:15:43 +0100

    first commit

---
+++ src/components/form.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form: React.FC = () => {
+  return (
+    <div>
+      <h3>JBook Demo</h3>
+      <form action=''>
+        <textarea name='' id='' cols='30' rows='10'></textarea>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
